225c1516a73c64d9fb00e4a51c2877a6c7d81e23
Mathmathg <gle.mathieu@gmail.com>
2019-01-13 17:59:02 +0100

    carctéristiques des personnages

--- js/app.js
+++ js/app.js
@@ -23,9 +23,36 @@ $('#people-btn').on('click', app.getPeople);
             var $li = $('<li>').text(character.name);
 
             $li.data('url', character.url);
-
+            $li.on('click', app.displayCharacter);
             $('#people-list').append($li);
+        });
+    },
+
+    displayCharacter: function(){
+        var url = $(this).data('url');
+        console.log(url);
+
+        // On charge la template
+        $.ajax('views/character.html', {
+            dataType: 'html'
         })
+        .done(function(response){
+            $.ajax(url + '?format=json')
+            .done(function(character){
+                console.log(character);
+
+                var $tpl = $(response);
+                $('.name', $tpl).text(character.name);
+                $tpl.find('.gender').text(character.gender);
+                $tpl.find('.height').text(character.height);
+                $tpl.find('.mass').text(character.mass);
+                $tpl.find('.hair_color').text(character.hair_color);
+                $tpl.find('.eye_color').text(character.eye_color);
+                $tpl.find('.birth_year').text(character.birth_year);
+               
+                $('#people-details').html($tpl.html());
+            })
+        });
     }
 };
 $(app.init);
